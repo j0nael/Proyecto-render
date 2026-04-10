51539875fafb2e758d52d73c79e46c6780d4e6b1
Jonathan Geraldo <157409634+j0nael@users.noreply.github.com>
2026-04-10 00:58:35 -0400

    add

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get("/", (req, res) => {
 });
 
 // Servir CSS
-app.use("/css", express.static(path.join(__dirname, "style.css")));
+app.use("/css", express.static(path.join(__dirname, "css", "styles.css")));
 
 // Servir JS
-app.use("/js", express.static(path.join(__dirname, "app.js")));
+app.use("/js", express.static(path.join(__dirname, "js", "app.js")));
 
 app.listen(PORT, () => {
     console.log("Servidor corriendo en puerto " + PORT);
